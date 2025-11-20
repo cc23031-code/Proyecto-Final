24c9aad448bf90b0d2a72699cb2d5e6a4be06261
Tania Cano <cc23031@ues.edu.sv>
2025-11-20 17:51:48 -0600

    Agrege contructor y destructor

--- Proyecto Final/cola.cpp
+++ Proyecto Final/cola.cpp
@@ -1,1 +1,21 @@
+#include "cola.h"
 
+// Constructor 
+Cola::Cola(){
+    frente=nullptr;
+    fin=nullptr;
+}
+//destructor
+cola::~Cola(){
+    while(!estaVacia()){
+        dequeue();
+    })
+}
+
+bool Cola::estaVacia(){
+    return frente==nullptr;
+}
+ 
+void Cola::enqueue(const Llamada& llamada){
+    
+}

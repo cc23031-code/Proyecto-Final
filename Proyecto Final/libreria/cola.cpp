cd187130f1cae99e0dcb7167231ea6a185153a94
cc23031@ues.edu.sv <cc23031@ues.edu.sv>
2025-11-25 11:01:08 -0600

    .cpp simulacionLlamada cambio

--- Proyecto Final/libreria/cola.cpp
+++ Proyecto Final/libreria/cola.cpp
@@ -66,6 +66,7 @@ void Cola::mostrarCola() const{
      std::cout << "\n========== COLA DE LLAMADAS ==========\n";
     Nodo* actual = frente;
     int posicion=1; // Posición de la llamada en la cola
+    int tiempoEsperaTotal = 0;// Tiempo de espera total acumulado
     int esperaAcumulada=0;
    std::string llamadas ;//=//"ID\t Nombre\t Motivo\t Tiempo de Espera \tDuracion Estimada"; // Encabezado de la tabla
   while (actual != nullptr) {
@@ -87,4 +88,8 @@ void Cola::mostrarCola() const{
 }
 
     std::cout<<llamadas<<"\n";
+    std::cout << "======================================\n";
+    std::cout << "Tiempo de espera total: " << tiempoEsperaTotal << " minutos\n";
+    std::cout << "Duracion total estimada: " << tiempoEspera << " minutos\n";
+    std::cout << "======================================\n";
 }

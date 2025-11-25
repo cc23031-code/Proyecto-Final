575a964d8b2b23a210c7925cfa1c061360b8b5de
cc23031@ues.edu.sv <cc23031@ues.edu.sv>
2025-11-24 23:28:44 -0600

    simulacion (entradas y salidas)

--- Proyecto Final/src/simulacionLlamada.cpp
+++ Proyecto Final/src/simulacionLlamada.cpp
@@ -1,6 +1,31 @@
-//#include <cola.h> // Incluye la definición de la clase Cola
+#include <cola.h>
 
 int main(){
+    Cola colaLlamadas;
+
+    // Simulación de agregar llamadas a la cola
+    Llamada llamada1 = {1, "Juan Perez", "Consulta de saldo", 0, 5};
+    Llamada llamada2 = {2, "Maria Lopez", "Reporte de tarjeta perdida", 2, 10};
+    Llamada llamada3 = {3, "Carlos Sanchez", "Cambio de plan", 4, 7};
+
+    colaLlamadas.enqueue(llamada1);
+    colaLlamadas.enqueue(llamada2);
+    colaLlamadas.enqueue(llamada3);
+
+    // Mostrar el estado de la cola
+    std::cout << "Estado inicial de la cola de llamadas:\n";
+     colaLlamadas.mostrarCola() ;
+
+    // Simulación de atender llamadas
+    std::cout << "Atendiendo llamadas...\n";
+    while (!colaLlamadas.estaVacia()) {
+        Llamada atendida = colaLlamadas.dequeue();
+        std::cout << "Atendiendo llamada ID: " << atendida.id << ", Nombre: " << atendida.nombre << "\n";
+    }
+
+    std::cout << "Todas las llamadas han sido atendidas.\n";
+
+    return 0;
 
 
 

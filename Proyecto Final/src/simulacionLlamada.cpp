5e7437c1be0e0da74bccd186a3c0b98dd97d08f0
cc23031@ues.edu.sv <cc23031@ues.edu.sv>
2025-11-25 18:06:16 -0600

    main modificado

--- Proyecto Final/src/simulacionLlamada.cpp
+++ Proyecto Final/src/simulacionLlamada.cpp
@@ -5,10 +5,11 @@ int main(){
 
     // Simulación de agregar llamadas a la cola
     Llamada llamada1 = {1, "Juan Perez", "Consulta de saldo", 0, 5};
+     colaLlamadas.enqueue(llamada1);
     Llamada llamada2 = {2, "Maria Lopez", "Reporte de tarjeta perdida", 2, 10};
     Llamada llamada3 = {3, "Carlos Sanchez", "Cambio de plan", 4, 7};
 
-    colaLlamadas.enqueue(llamada1);
+    
     colaLlamadas.enqueue(llamada2);
     colaLlamadas.enqueue(llamada3);
 
